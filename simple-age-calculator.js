e3783a3fd649b1be633542807e2841ff1c14a040
princemaxi <princemaxwellugochukwu@gmail.com>
2023-07-30 16:20:59 +0100

    my simple age calculator is ready

--- simple-age-calculator.js
+++ simple-age-calculator.js
@@ -1,8 +1,12 @@
+
 let cur_year= parseInt(prompt("Enter Current Year:"));
 let birth_year = parseInt(prompt("Enter Your Birth Year:"));
 
 function ageCalculator() {
-    return cur_year - birth_year;
+
+let my_output =  cur_year - birth_year;
+    
+    return my_output;
 }
 
 console.log(ageCalculator());
