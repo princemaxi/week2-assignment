135168c5287019c5503ed66ed84e60ee705e8855
princemaxi <princemaxwellugochukwu@gmail.com>
2023-07-30 16:35:58 +0100

    my week 2 assignment is completed

--- simple-age-calculator.js
+++ simple-age-calculator.js
@@ -1,12 +1,41 @@
+// defining our variables
+const birthYear = parseInt(prompt("Enter your birth year"));;
+const currentYear = parseInt(prompt("Enter current year"));;
 
-let cur_year= parseInt(prompt("Enter Current Year:"));
-let birth_year = parseInt(prompt("Enter Your Birth Year:"));
+function calculateAge(birthYear, currentYear) {
+    if (currentYear < birthYear) {
+      return "Invalid input: Current year cannot be before birth year.";
+    }
+  
+    const age = currentYear - birthYear;
+    return age;
+  }
+  
+  
+  const age = calculateAge(birthYear, currentYear);
+  console.log("Your age is: " + age);
 
-function ageCalculator() {
 
-let my_output =  cur_year - birth_year;
-    
-    return my_output;
+
+
+// EVEN NUMBERS
+//  Define an array called numbersArray, which contains numbers from 1 to 20. 
+// We then create a function called isEven() that takes a number as input and returns true if it's even and false otherwise. 
+// Inside the loop, we check each number in the array using the isEven() function and use console.log() to output only the even numbers 
+// (2, 4, 6, 8, 10) to the console.
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// Function to check if a number is even
+function isEven(number) {
+  return number % 2 === 0;
 }
 
-console.log(ageCalculator());
+// Loop through the array and output even numbers
+console.log("Even numbers in the array:");
+for (let i = 0; i < numbersArray.length; i++) {
+  const currentNumber = numbersArray[i];
+  if (isEven(currentNumber)) {
+    console.log(currentNumber);
+  }
+}
